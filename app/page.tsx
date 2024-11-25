ca94c2dce48144eab7aeca83a6dd07e17698d38e
nilsdacke1 <nilsdacke1@gmail.com>
2024-11-25 21:08:24 +0100

    env check

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  const podname = process.env.NEXT_PUBLIC_POD_NAME || "unknown";
+  const podname = process.env.POD_NAME || "unknown";
   return (
     <div className="grid min-h-screen justify-center items-center">
       Tobias Berg
