9174440d21a8eab6ba62f2abc8bfef229b99240a
nilsdacke1 <nilsdacke1@gmail.com>
2024-11-25 20:48:28 +0100

    add podname to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+export default async function Home() {
+  const podname = process.env.POD_NAME || "unknown";
   return (
     <div className="grid min-h-screen justify-center items-center">
       Tobias Berg
+      <p> Currently running on pod {podname}</p>
     </div>
   );
 }
