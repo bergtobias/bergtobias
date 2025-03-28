16c7d9937551ba3c159ef0fb3c9a09734b45f915
Tobias Berg <tobiaswillyberg@gmail.com>
2025-03-28 20:34:54 +0100

    update

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,13 +1,23 @@
+import React, { Suspense } from "react";
 import Main from "./Main";
 import { Sidebar } from "./Sidebar";
 import Toolbar from "./Toolbar";
 
-export default function CV() {
+function CVContent() {
+  // Assuming useSearchParams is used in one of these components
   return (
-    <div className="print:w-[210mm] print:aspect-[210/297] max-w-4xl mx-auto bg-background  grid md:grid-cols-[2fr_5fr] h-[screen-2rem] print:border-0 border print:rounded-none rounded-lg shadow-md print:shadow-none">
+    <div className="print:w-[210mm] print:aspect-[210/297] max-w-4xl mx-auto bg-background grid md:grid-cols-[2fr_5fr] h-[screen-2rem] print:border-0 border print:rounded-none rounded-lg shadow-md print:shadow-none">
       <Sidebar />
       <Main />
       <Toolbar />
     </div>
+  );
+}
+
+export default function CV() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CVContent />
+    </Suspense>
   );
 }
